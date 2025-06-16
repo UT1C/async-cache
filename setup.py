d6402921ecd0b9838344040fe5283688a705f4db
lightmanLP <liteman1000@gmail.com>
2025-06-16 09:23:06 +0700

    meta

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="async-cache",
-    version="1.1.1",
-    author="Rajat Singh",
-    author_email="iamsinghrajat@gmail.com",
+    version="2.0.0",
+    author="UT1C",
+    author_email="liteman1000@gmail.com",
     description="An asyncio Cache",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.com/iamsinghrajat/async-cache",
+    url="https://github.com/UT1C/async-cache",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.3",
+    python_requires=">=3.10",
     keywords=["asyncio", "lru", "cache", "async", "cache", "lru-cache", "ttl"],
 )
